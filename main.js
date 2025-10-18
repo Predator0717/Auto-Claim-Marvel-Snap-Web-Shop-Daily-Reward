fd5bf9058a12cf2cd9b9ecf608f4e02df903bb16
Predator0717 <paradisefalls0717@gmail.com>
2025-10-18 18:01:18 +0800

    Add userscript for auto claiming daily reward

---
+++ main.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Auto Claim Marvel Snap Web Shop Daily Reward
+// @match        https://shop.marvelsnap.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function clickButton() {
+        let btn = document.querySelector("#buy-button-dailyfree100credits");
+        if (btn) {
+            btn.click();
+            console.log("✅ Credit received");
+        } else {
+            console.log("❌ button note found");
+        }
+    }
+
+    // 页面加载后延迟执行
+    window.addEventListener('load', () => {
+        setTimeout(clickButton, 3000);
+    });
+})();
